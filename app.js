cf66cd7785a2f2904a00be755eb75df87c54cfb7
Tom <huan1043269994@gmail.com>
2017-08-22 10:26:55 +0800

    add route for html and cache static asset

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const views = require('co-views');
 const route = require('koa-route');
+const serve = require('koa-static');
+
+const fs = require('fs');
+const path = require('path');
 
 const Koa = require('koa');
 const app = new Koa();
 
-const render = views('views', {
-  ext: 'ejs',
+const render = views(__dirname + '/views', {
+  map: { html: 'ejs' }
 });
 
 
-app.use(route.get('/'), main);
-app.use(route.get())
+const main = async ctx => {
+  ctx.response.body = await render('index');
+};
+
+const shop = async ctx => {
+  ctx.response.body = await render('shop');
+};
+
+const assets = serve(path.join(__dirname));
+
+app.use(assets);
+app.use(route.get('/', main));
+app.use(route.get('/shop', shop));
+
 
 app.listen(3000);
