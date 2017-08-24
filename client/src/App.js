fcaf32bd126ccdb80e78a8e4a6602f4c16b37376
Tom <huan1043269994@gmail.com>
2017-08-24 17:13:00 +0800

    modify for animated

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './css/App.css';
+import './css/slippry.css'
+import carousel1 from './img/carousel-1.jpg';
+import carousel2 from './img/carousel-2.jpg';
+import carousel3 from './img/carousel-3.jpg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <ul ref={(ul) => { this.slider = ul; }} id="slider" style={{listStyle: 'none', width: '100%', height: '100%', padding: 0, margin: 0}} className="hde">	
+            <li><img src={carousel1} alt="轮播图1"/></li>
+            <li><img src={carousel2} alt="轮播图2"/></li>
+            <li><img src={carousel3} alt="轮播图3"/></li>   
+          </ul>
       </div>
     );
   }
