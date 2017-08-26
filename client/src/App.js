e6163d23511f5adedd6a875a3a108dfe63736734
powerformer <huan1043269994@gmail.com>
2017-08-26 12:04:11 +0800

    home page basic completed

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,10 @@ import carousel3 from './img/carousel-3.jpg';
 import videoCover from './img/video-cover.jpg';
 import map from './img/map.jpg';
 
-import goods1 from './img/goods-1.jpg';
-import goods2 from './img/goods-2.jpg';
-import goods3 from './img/goods-3.jpg';
+import goods1 from './img/goods-1.png';
+import goods2 from './img/goods-2.png';
+import goods3 from './img/goods-3.png';
+import goods4 from './img/goods-4.png';
 
 import commit1 from './img/commit-1.svg'
 import commit2 from './img/commit-2.svg'
@@ -86,10 +87,10 @@ class App extends Component {
           </div>
 
           <div id="about" className="container-fluid culture">
-            <div className="row">
+            <div className="row culutre-box">
               <div className="col-sm-7">
-                <h2>我们的文化</h2>
-                <p>相比之前的传输协议，HTTP/2在底层方面做了很多优化。有安全、省时、简化开发、更好的适应复杂页面、提供缓存利用率等优势，阿里云早在去年发布的CDN6.0服务就已正式支持HTTP/2，访问速度最高可提升68%。今天我们从历史、特性、调试、性能四个层面来全面解析HTTP/2。</p>
+                <h3 className="culture-title text-center">我们的文化</h3>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;相比之前的传输协议，HTTP/2在底层方面做了很多优化。有安全、省时、简化开发、更好的适应复杂页面、提供缓存利用率等优势，阿里云早在去年发布的CDN6.0服务就已正式支持HTTP/2，访问速度最高可提升68%。今天我们从历史、特性、调试、性能四个层面来全面解析HTTP/2。</p>
               </div>
               <div className="col-sm-5">
                 <img src={videoCover} alt="videoCover" className="video-cover"/>
@@ -97,69 +98,68 @@ class App extends Component {
             </div>
           </div>
 
-          <div id="news" className="container-fluid">
-            <div className="row">
+          <div id="news" className="container-fluid culture map-box">
+            <div className="row culutre-box">
               <div className="col-sm-5">
                 <img src={map} alt="map" className="map"/>
               </div>
               <div className="col-sm-7">
-              <h2>盐津简介</h2>
+              <h3 className="culture-title text-center">在盐津，放飞梦想</h3>
               <p>云南省盐津县，位于云南省东北部，昭通市下辖县，东华大学对口帮扶的国家级贫困县。盐津属典型的山区县，优越的地理气候条件，造就了盐津良好的生态环境。盐津自古是入滇的要冲，素有“咽喉西蜀、锁钥南滇”之称，地处乌蒙山脉关河深谷的中段，位于四川和云南结合之处，也是古丝绸之路入滇的第一站。全县有着丰富的自然资源、独特的区位优势、厚重的人文情怀，更有着乌骨鸡、天麻、竹笋、腊肉、白酒等品种多样、品质优良、健康生态的众多农特产品。</p>
             </div>
             </div>
           </div>
 
           <div id="shop" className="container-fluid goods">
-            <h2 className="sector-title">部分商品展示</h2>
-          </div>
-
-          <div className="container-fluid text-center bg-grey">
-            <div className="row text-center">
-              <div className="col-sm-4">
+            <h3 className="goods-title text-center">部分商品展示</h3>
+            <div className="row text-center scrolls">
+              <div className="col-sm-3">
                 <div className="thumbnail">
                   <img src={goods1} alt="good1" className="goodItem"/>
                   <p><strong>Paris</strong></p>
-                  <p>Yes, we built Paris</p>
+                  <p>一口忘忧</p>
                 </div>
               </div>
-              <div className="col-sm-4">
+              <div className="col-sm-3">
                 <div className="thumbnail">
                   <img src={goods2} alt="good2" className="goodItem"/>
                   <p><strong>New York</strong></p>
-                  <p>We built New York</p>
+                  <p>纯天然酿造，不含任何化学物质</p>
                 </div>
               </div>
-              <div className="col-sm-4">
+              <div className="col-sm-3">
                 <div className="thumbnail">
                   <img src={goods3} alt="good3" className="goodItem"/>
                   <p><strong>San Francisco</strong></p>
-                  <p>Yes, San Fran is ours</p>
+                  <p>没有最丑，只有更丑</p>
                 </div>
               </div>
+
+              
               </div>
           </div>
 
 
           <div className="container-fluid commitments">
-            <h2 className="sector-title">我们的承诺</h2>
+            <h3 className="goods-title text-center commitments-title">我们的承诺</h3>
             <div className="col-sm-6 commit">
               <img src={commit1} alt="commit-1" /><br />
-              <h3>正品保证</h3>
+              <h4>正品保证</h4>
               <p>严格控制供应渠道，全部商品均由品牌供应商直接采购供货，品质有保障。</p>
             </div>
             <div className="col-sm-6 commit">
               <img src={commit2} alt="commit-2" /><br />
-              <h3>支持退换货</h3>
+              <h4>支持退换货</h4>
               <p>支持退换货，退换货请在商品收到的七天内进行退换货申请，并将商品寄回。</p>
             </div>
-            <div className="col-sm-6 commit">
+            <div className="col-sm-6 commit commit-bottom">
               <img src={commit3} alt="commit-3" /><br />
-              <h3>尽快发货</h3>
+              <h4>尽快发货</h4>
               <p>我们会尽快安排发货，在发货后一般会在2-7天左右送达。</p>
             </div>
-            <div className="col-sm-6 commit">
+            <div className="col-sm-6 commit commit-bottom">
               <img src={commit4} alt="commit-4" /><br />
-              <h3>关于色差</h3>
+              <h4>关于色差</h4>
               <p>所卖商品均为实物拍摄，因此难免会由于灯光等原因出现略微色差，一切以实物为准。</p>
             </div>
           </div>
