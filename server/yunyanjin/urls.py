2f04f3cc023fc6129b547b9f4ce6bf6a2d7db32b
mRcfps <1402491442@qq.com>
2017-08-28 22:54:11 +0800

    Server evolution.

--- server/yunyanjin/urls.py
+++ server/yunyanjin/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^users/', include('users.urls', namespace='users')),
     url(r'^shop/', include('shop.urls', namespace='shop')),
     url(r'^cart/', include('cart.urls', namespace='cart')),
+    url(r'^orders/', include('orders.urls', namespace='orders')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
