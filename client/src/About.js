c4b7d0362bb72fb4e225876f43754b59eb8b1c7e
powerformer <huan1043269994@gmail.com>
2017-08-30 14:17:06 +0800

    add shadow to video-cover

--- client/src/About.js
+++ client/src/About.js
@@ -4,6 +4,8 @@ import './css/rubick_pres.css';
 import './css/App.css';
 import './css/modal.css';
 
+import './css/About.css';
+
 import videoCover from './img/video-cover.jpg';
 
 class About extends Component {
@@ -15,7 +17,7 @@ class About extends Component {
                 <h3 className="culture-title text-center">我们的文化</h3>
                 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;相比之前的传输协议，HTTP/2在底层方面做了很多优化。有安全、省时、简化开发、更好的适应复杂页面、提供缓存利用率等优势，阿里云早在去年发布的CDN6.0服务就已正式支持HTTP/2，访问速度最高可提升68%。今天我们从历史、特性、调试、性能四个层面来全面解析HTTP/2。</p>
               </div>
-              <div className="col-sm-5">
+              <div className="col-sm-5 box-img-shadow">
                 <img src={videoCover} alt="videoCover" className="video-cover"/>
               </div>
             </div>
