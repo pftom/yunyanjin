e86f7444658f150b8ec0cf782652436adfca105d
powerformer <huan1043269994@gmail.com>
2017-08-31 19:25:22 +0800

    header animation

--- client/src/App.js
+++ client/src/App.js
@@ -104,6 +104,12 @@ class App extends Component {
 
     return (
       <div>
+          <ul id="slider" style={{ zIndex: -1, position: 'absolute', listStyle: 'none', width: '100%', height: '100%', padding: 0, margin: 0 }} className="hde">	
+                  <li><img src="http://yunyanjin.oss-cn-hangzhou.aliyuncs.com/slippry/s1.jpg" /></li>
+                  <li><img src="http://yunyanjin.oss-cn-hangzhou.aliyuncs.com/slippry/s2.jpg" /></li>
+                  <li><img src="http://yunyanjin.oss-cn-hangzhou.aliyuncs.com/slippry/s3.jpg" /></li>
+                  
+              </ul>
           <NavBar 
             isLoggedIn={this.state.isLoggedIn}
             showLoginModal={this.showModal}
--- client/src/HeaderBody.js
+++ client/src/HeaderBody.js
@@ -13,21 +13,10 @@ import carousel3 from './img/carousel-3.jpg';
 class HeaderBody extends Component {
     render() {
         return (
-            <div className="header-body" id="topNav">
-            <div id="myCarousel" className="carousel slide" data-ride="carousel">
-              <div className="carousel-inner" role="listbox">
-                <div className="item active">
-                  <img src={carousel1} alt="carousel1" />
-                </div>
-              </div>
-                <div className="title-board">
-                  <div className="title-box">
-                    <h1 className="project-title">云梦盐津</h1>
-                    <p className="project-desc">梦想，在这里发生</p>
-                  </div>
-                </div>
+            <div className="header-body" id="topNav" style={{ height: '100vh', zIndex: 3 }}>
+              <h1 className="project-title">云梦盐津</h1>
+              <p className="project-desc">梦想，在这里发生</p>
             </div>
-          </div>
         )
     }
 }
