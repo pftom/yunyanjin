3549d9fd948096276f5c36bc692fd7db19966576
黄巍 <huan1043269994@gmail.com>
2017-09-07 16:09:33 +0800

    add new source video

--- client/src/ExtraVideo.js
+++ client/src/ExtraVideo.js
@@ -7,7 +7,7 @@ export default (props) => {
     <Player
       playsInline
       poster='http://yunyanjin.oss-cn-hangzhou.aliyuncs.com/video-cover.png'
-      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+      src="http://yunyanjin.oss-cn-hangzhou.aliyuncs.com/%E4%BA%91%E6%A2%A6%E7%9B%90%E6%B4%A5%E8%B0%83%E7%A0%94%E5%AE%A3%E4%BC%A0%E8%A7%86%E9%A2%91.mp4"
     />
   );
 };
