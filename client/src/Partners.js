ad5241670baf3957e744bb8bd0660ba67416956e
黄巍 <huan1043269994@gmail.com>
2017-09-10 14:10:59 +0800

    adapt for responsive

--- client/src/Partners.js
+++ client/src/Partners.js
@@ -5,23 +5,34 @@ import { data } from './data'
 
 import { Table, Button } from 'antd';
 
+const fixIt = {
+    
+}
 
 // con
 const columns = [{
     title: '企业名称',
     dataIndex: 'firmName',
+    key: 'firmName',
 }, {
     title: '企业电话',
     dataIndex: 'firmPhone',
-}, {
-    title: '联络员姓名',
-    dataIndex: 'personName',
-}, {
-    title: '联络员电话',
-    dataIndex: 'personPhone',
+    key: 'firmPhone',
 }, {
     title: '企业地址',
     dataIndex: 'address',
+    key: 'address',
+},
+{
+    title: '联络员姓名',
+    dataIndex: 'personName',
+    key: 'personName',
+}, {
+    title: '联络员电话',
+    dataIndex: 'personPhone',
+    key: 'personPhone',
+    fixed: 'right',
+    width: 100
 }]
 
 class Partners extends Component {
@@ -46,7 +57,7 @@ class Partners extends Component {
             <div className="container-fluid partner" id="partner">
                 <h3 className="sector-title text-center">合作伙伴</h3>
                 <div className="tab-box">
-                    <Table columns={columns} dataSource={data} />
+                    <Table columns={columns} dataSource={data} scroll={{ x: 1300 }} />
                 </div>
             </div>
         )
