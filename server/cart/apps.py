8416a43381b78f90179afecd1537192d7fbe5661
mRcfps <1402491442@qq.com>
2017-10-17 15:41:08 +0800

    Remove server-side codebase.

--- server/cart/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CartConfig(AppConfig):
-    name = 'cart'
-    verbose_name = '购物车'
--- server/shop/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShopConfig(AppConfig):
-    name = 'shop'
-    verbose_name = '在线商城'
